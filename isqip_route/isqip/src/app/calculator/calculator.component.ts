10438147b3caf43e7661e2c3260d425c43766df5
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2019-07-08 15:56:06 +0530

    Value Passing

--- isqip_route/isqip/src/app/calculator/calculator.component.ts
+++ isqip_route/isqip/src/app/calculator/calculator.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  public calcInput : string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  btnClicked(val){
+    console.log("this is",this.calcInput);
+  }
 }
+  
